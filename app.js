d524da9a8d1515a7f171b991403fe87848fe18c1
Jenny Lin <misslinyang91@gmail.com>
2021-08-19 14:56:53 +1000

    added printput function in the textarea window

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ let textArea = document.querySelector(".display textarea");
 
 keyContainers.forEach(function (key) {
   key.addEventListener("click", function (e) {
-    console.log(e.path[0].value);
+    //console.log(e.path[0].value.toLowerCase());
+
+    if (!e.path[0].value) return;
+
+    let keyClicked = e.path[0].value.toLowerCase();
+    textArea.value += keyClicked;
   });
 });
